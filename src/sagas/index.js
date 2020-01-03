25cc95c7f8c08d39549bf5c81715903743943987
Benjamin Huang <bhuang@rms.com>
2020-01-03 10:34:47 -0800

    add async saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,13 @@ import { all } from 'redux-saga/effects'
 //
 import { fetchUsersWatcher } from './user'
 import { wathIncrementAsync } from './counter'
+import { watchAgeUp} from './people'
 
 
 export function* rootSaga() {
   yield all([
     fetchUsersWatcher(),
-    wathIncrementAsync()
+    wathIncrementAsync(),
+    watchAgeUp()
   ])
 }
